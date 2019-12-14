9d09b6d0f28bc50ea45304573c1e70dbb0ad66d1
robert.king@radiancetech.com <kingbob2015@hotmail.com>
2019-12-14 17:22:47 -0500

    added some comments and some other changes

--- pieye/app.py
+++ pieye/app.py
@@ -3,26 +3,32 @@ import os
 from config.config import Config
 from log.logger import Logger, LogLevel
 
+
 def run():
-    # TODO: Check for config file and add some error handling
+    """
+    Main loop for the program
+    """
+
+    # Create our logger singleton instance for the rest of the application to use
     logger = Logger.get_instance()
-    logger.log("test message", LogLevel.INFO)
 
+    # Load up our config to pass to components that need it
     config = Config(os.path.dirname(__file__)+"/config.json")
-
     camera = TempCamera()
     while True:
         frame = camera.next_frame()
         if temp_motion_detect(frame):
             temp_message_system(config.get_config("messaging"), "Bob", "Hey Bob Some Motion Was Detected")
 
+        # TEMP CODE FOR SHOWING AN IMAGE
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         cv2.imshow('frame', gray)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        # END TEMP CODE
 
-    camera.__del__()
+    camera.delete()
 
 # TEMP CODE THAT WILL BE SUBBED FOR REAL CODE
 
@@ -32,8 +38,10 @@ def temp_motion_detect(frame):
 
 
 def temp_message_system(config, name, message):
-    print(config)
-    print("Sending {} to {} at {}".format(message, name, config["names_to_numbers"][name]))
+    # Would need some error handling here in actual messaging system
+    if config:
+        print(config)
+        print("Sending {} to {} at {}".format(message, name, config["names_to_numbers"][name]))
 
 
 class TempCamera:
@@ -51,8 +59,12 @@ class TempCamera:
         self.__cam.release()
         cv2.destroyAllWindows()
 
+    def delete(self):
+        self.__del__()
+
 # END TEMP CODE
 
 
 if __name__ == '__main__':
+    # TODO: Add in command line parameters to Python module for config file path and such
     run()
--- pieye/log/logger.py
+++ pieye/log/logger.py
@@ -1,18 +1,32 @@
 import os
 import enum
 
-# TODO: Error handling for file stuff
 
 class LogLevel(enum.Enum):
+    """
+    The LogLevel class contains the enums for log levels. INFO, WARN, or ERROR
+    """
     INFO = 1
     WARN = 2
     ERROR = 3
 
 class Logger:
+    """
+    The Logger class  singleton that can take parameters for log level from the LogLevel class and log file path
+    Only access the Logger class via get_instance
+    """
     __instance, __file, __logLevel = None, None, None
 
     @staticmethod
     def get_instance(log_level=LogLevel.INFO, log_file=os.path.dirname(__file__) + "/../log.txt"):
+        """
+        Gets the instance of Logger if it has been instantiated or creates one
+
+        @param: log_level: the level of logging, takes from the enum of LogLevel
+        @param: log_file: the file to log to
+
+        @return: The instance of the singleton Logger
+        """
         if Logger.__instance is None:
             Logger(log_level, log_file)
         return Logger.__instance
@@ -28,6 +42,12 @@ class Logger:
             Logger.__instance = self
 
     def log(self, log_message, log_level):
+        """
+        Logs a message
+
+        @param: log_message: the message to log to the file that the Logger was instantiated with
+        @param log_level: the level that the log message is logging for from the enum of LogLevel
+        """
         if log_level.value >= Logger.__logLevel.value:
             with open(self.__file, "a") as file:
                 file.write(log_message + "\n")
